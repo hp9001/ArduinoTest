0512cb46970f9c8cbab99b60c31635fbf9ed71ed
Luigi Santagada <luigi.santagada@live.com>
2018-10-24 15:30:27 +0200

    .

---
+++ ArduinoTest/Debug/core/Tone.cpp.d
@@ -0,0 +1,13 @@
+C:\Users\luigi.santagada\AppData\Local\Temp\VMBuilds\ArduinoTest\nano_atmega328\Debug\core\Tone.cpp.o: \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Tone.cpp \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Arduino.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\binary.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\WCharacter.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\WString.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\HardwareSerial.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Stream.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Print.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Printable.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\USBAPI.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h
---
+++ ArduinoTest/Debug/core/WString.cpp.d
@@ -0,0 +1,3 @@
+C:\Users\luigi.santagada\AppData\Local\Temp\VMBuilds\ArduinoTest\nano_atmega328\Debug\core\WString.cpp.o: \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\WString.cpp \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\WString.h
