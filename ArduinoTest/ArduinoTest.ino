2ab2052e0cf426fa6ebfdb3eb633734adf3d1f98
Luigi Santagada <luigi.santagada@live.com>
2018-10-25 09:50:37 +0200

    .

--- ArduinoTest/ArduinoTest.ino
+++ ArduinoTest/ArduinoTest.ino
@@ -7,11 +7,9 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
 
-	Serial(222);
-	
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {   
-  xxxxxxxxxxxxxx
+ 
 }
