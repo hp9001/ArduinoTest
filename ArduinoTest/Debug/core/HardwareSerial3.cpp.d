145cd660b7abfecc469b9d160d8fd8f67bd699b1
Luigi Santagada <luigi.santagada@live.com>
2018-10-29 13:51:05 +0100

    .

--- ArduinoTest/Debug/core/HardwareSerial3.cpp.d
+++ ArduinoTest/Debug/core/HardwareSerial3.cpp.d
@@ -1,4 +1,4 @@
-C:\Users\luigi.santagada\AppData\Local\Temp\VMBuilds\ArduinoTest\attiny_ATtinyX5_attiny25\Debug\core\HardwareSerial3.cpp.o: \
+C:\Users\luigi.santagada\AppData\Local\Temp\VMBuilds\ArduinoTest\uno\Debug\core\HardwareSerial3.cpp.o: \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\HardwareSerial3.cpp \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Arduino.h \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\binary.h \
@@ -9,6 +9,6 @@ C:\Users\luigi.santagada\AppData\Local\Temp\VMBuilds\ArduinoTest\attiny_ATtinyX5
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Print.h \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Printable.h \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\USBAPI.h \
- C:\Users\luigi.santagada\AppData\Local\arduino15\packages\attiny\hardware\avr\1.0.2\variants\tiny8/pins_arduino.h \
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\variants\standard/pins_arduino.h \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\HardwareSerial_private.h \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\wiring_private.h
--- ArduinoTest/Debug/core/Tone.cpp.d
+++ ArduinoTest/Debug/core/Tone.cpp.d
@@ -1,4 +1,4 @@
-C:\Users\luigi.santagada\AppData\Local\Temp\VMBuilds\ArduinoTest\attiny_ATtinyX5_attiny25\Debug\core\Tone.cpp.o: \
+C:\Users\luigi.santagada\AppData\Local\Temp\VMBuilds\ArduinoTest\uno\Debug\core\Tone.cpp.o: \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Tone.cpp \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Arduino.h \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\binary.h \
@@ -9,4 +9,4 @@ C:\Users\luigi.santagada\AppData\Local\Temp\VMBuilds\ArduinoTest\attiny_ATtinyX5
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Print.h \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\Printable.h \
  C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\cores\arduino\USBAPI.h \
- C:\Users\luigi.santagada\AppData\Local\arduino15\packages\attiny\hardware\avr\1.0.2\variants\tiny8/pins_arduino.h
+ C:\Program\ Files\ (x86)\arduino-1.8.5\hardware\arduino\avr\variants\standard/pins_arduino.h
